7131c2cf26946c6a8415dc99524f8e9339a2e8a9
forestdussault <forest.dussault@inspection.gc.ca>
2018-04-09 10:21:44 -0400

    Bug fix, removed unnecessary dependencies

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="AutoInterOp",
-    version="0.1.3",
+    version="0.1.4",
     packages=find_packages(),
     author="Forest Dussault",
     author_email="forest.dussault@inspection.gc.ca",
     url="https://github.com/forestdussault/AutoInterOp",
     scripts=['AutoInterOp.py'],
-    install_requires=['biopython', 'OLCTools', 'click', 'numpy']
+    install_requires=['click', 'numpy']
 )
